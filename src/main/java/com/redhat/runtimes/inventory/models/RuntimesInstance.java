9ba3c54a736d795bbae0f796768594a88b036b73
Ben Evans <benjamin.john.evans@gmail.com>
2023-02-01 10:58:08 +0000

    Add data fieldas

--- src/main/java/com/redhat/runtimes/inventory/models/RuntimesInstance.java
+++ src/main/java/com/redhat/runtimes/inventory/models/RuntimesInstance.java
@@ -24,6 +24,40 @@ public class RuntimesInstance {
   @Size(max = 50)
   private String hostname;
 
+//   "java.vm.specification.vendor" : "Oracle Corporation",
+  @NotNull
+  @Size(max = 255)
+  private String vendor;
+
+//      "java.runtime.version" : "17.0.1+12",
+  @NotNull
+  @Size(max = 255)
+  private String versionString;
+
+//      "java.version" : "17.0.1",
+  @NotNull
+  @Size(max = 255)
+  private String version;
+
+//    "java.vm.specification.version" : "17",
+  @NotNull
+  private int majorVersion;
+
+//  "os.arch" : "x86_64",
+  @NotNull
+  @Size(max = 50)
+  private String osArch;
+
+//      "Logical Processors" : 12,
+  @NotNull
+  private int processors;
+
+//  "Heap max (MB)" : 8192.0,
+  @NotNull
+  private double heapMax;
+
+  //////////////////////////////////////////////////////
+
   public void setId(String id) {
     this.id = id;
   }

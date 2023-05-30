6a9eeb74fae8c28249a1827bd34f289622b42813
Ben Evans <benjamin.john.evans@gmail.com>
2023-05-30 10:36:32 +0000

    Move to Quarkus 3 RC1 (#27)
    * Remove unused properties
    
    * RED: Compiles, but won't package
    
    * Remove unnecessary dependencies
    
    * Fix Cloudwatch version

--- events/src/main/java/com/redhat/runtimes/inventory/events/ArchiveAnnouncementParser.java
+++ events/src/main/java/com/redhat/runtimes/inventory/events/ArchiveAnnouncementParser.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import javax.enterprise.context.ApplicationScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public final class ArchiveAnnouncementParser {

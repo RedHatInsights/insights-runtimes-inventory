7768060e2e4be75ee2d69d01da73c89a45a9841c
Ben Evans <benjamin.john.evans@gmail.com>
2023-05-31 17:00:13 +0200

    RC4

---
+++ events/src/test/java/com/redhat/runtimes/inventory/events/PersistTest.java
@@ -0,0 +1,12 @@
+/* Copyright (C) Red Hat 2023 */
+package com.redhat.runtimes.inventory.events;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+public class PersistTest {
+
+  @Test
+  @Disabled
+  public void testPersist() {}
+}

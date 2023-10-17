6986b227b697048653426c47bc4124e5a2e532fe
Alex Macdonald <almacdon@redhat.com>
2023-10-17 09:17:32 -0400

    Run maven wrapper in the unit test script

--- unit_test.sh
+++ unit_test.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
+# https://github.com/RedHatInsights/cicd-tools/blob/main/examples/unit_test_example.sh
+
+# Run unit tests
+./mvnw clean package
+result=$?
+
+# Evaluate the test result.
+
+# If your unit tests store junit xml results, you should store them in a file matching format `artifacts/junit-*.xml`
 mkdir -p artifacts
-cat << EOF > artifacts/junit-dummy.xml
-<testsuite tests="1">
-    <testcase classname="dummy" name="dummytest"/>
-</testsuite>
-EOF
+for directory in "core" "events"; do # currently no "unit" test in /rest
+    cp $directory/target/surefire-reports/TEST-*.xml artifacts
+done
+
+for file in $(ls artifacts/); do
+    mv artifacts/$file artifacts/junit-$file
+done
+
+if [ $result -ne 0 ]; then
+  echo '====================================='
+  echo '====  ✖ ERROR: UNIT TEST FAILED  ===='
+  echo '====================================='
+  exit 1
+fi

aac6fd16cc12396e3a952f4049ff11429ec5a89f
Ben Evans <beevans@redhat.com>
2024-02-05 19:26:17 +0100

    Prepare for archive

--- runtimes_inventory_ephemeral.sh
+++
@@ -1,5 +0,0 @@
-# export TEMP_INSIGHTS_SERVER=https://env-ephemeral-zdazhn-urtnxyqd.apps.c-rh-c-eph.8p0c.p1.openshiftapps.com
-# export TEMP_INSIGHTS_TOKEN=amRvZTpwZlFvUU5kc0xEc2NlaUU5
-
-curl -H "Authorization: Basic ${TEMP_INSIGHTS_TOKEN}" \
- "${TEMP_INSIGHTS_SERVER}/api/runtimes-inventory/v1/instance/?hostname=${HOSTNAME}" -v
